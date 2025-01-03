0d3d644794e76c0434e224a6a16f619b0f24a90f
FilippSM_laptop <philippsm666@gmail.com>
2025-01-03 12:47:04 +0300

    last version

--- src/sections/setCounter/SetCounter.tsx
+++ src/sections/setCounter/SetCounter.tsx
@@ -8,17 +8,30 @@ type SetCounterType = {
 }
 
 export const SetCounter = (props: SetCounterType) => {
-    const maxValue = 0;
+    const maxValue = 5;
     const minValue = 0;
 
     const [countMax, setCountMax] = useState<number>(maxValue);
     const [countMin, setCountMin] = useState<number>(minValue);
 
+    const [error, setError] = useState<any>(0);
 
     const setNumbers = () => {
-        props.getNumbers(countMax, countMin)
+        props.getNumbers(countMax, countMin) 
+        setError(countMin)
     };
 
+
+    const renderMessage = () => {
+        if (countMin < 0) {
+          
+            return <div>incorrect value</div>;
+        }
+        if (countMin === countMax) {
+            return <div>incorrect value</div>;
+        }
+        return <div>Enter values and press set</div>;
+    };
 
     return (
         <div className={styles.box}>
@@ -34,8 +47,13 @@ export const SetCounter = (props: SetCounterType) => {
                 <Button callBack={setNumbers} name={"set"} /* disabled={count >= maxValue} */ />
             </div>
            {/*  для проверки */}
-            {/* <div>max value: {countMax}</div>
-            <div>start value: {countMin}</div> */}
+            <div>max value: {countMax}</div>
+            <div>start value: {countMin}</div>
+
+            <div>{renderMessage()}</div>
+            <div>{error}</div>
+        {/*     {countMin < 0 ? 0 : <div>Enter values and press set</div>} */}
+      
         </div>
     )
 }
